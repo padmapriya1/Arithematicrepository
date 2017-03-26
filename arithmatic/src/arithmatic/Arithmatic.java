1a1b6cddbd6ab63ad24da2e06050e86d381bfc14
padmapriya1 <padmapriyagsv123@gmail.com>
2017-03-26 23:51:15 +0530

    hi

---
+++ arithmatic/src/arithmatic/Arithmatic.java
@@ -0,0 +1,86 @@
+package arithmatic;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+		
+			int a=78,b=44;
+		//Arithmetic operator +,-,*,/,%     ++ (post increment  pre increment) --(predecrement and post decrement)
+			
+				System.out.println("addition is= "+(a+b));//addition is:392 
+				
+				
+				System.out.println( "division ="+(a/b) );//5
+				System.out.println( "modulus is="+(a%b) );//0
+				
+				int c= a++;//post increment 
+				System.out.println(c);//78
+				System.out.println(a);//79
+				
+				c=++a;
+				
+				System.out.println(c);//80
+				System.out.println(a);//80
+				
+				int d=b--;
+				System.out.println(d);//44
+				System.out.println(b);//43
+				
+				d=--b;
+				System.out.println(d);//42
+				System.out.println(b);//
+				
+				
+				int e=300;  
+				
+				
+				e/=100;//e=e/100
+				System.out.println(e);//3
+				
+				
+				/*
+				//post fix increment operator  a++
+				
+				System.out.println(a++);//45
+				System.out.println(a);//46
+				System.out.println(a++);//46
+		
+		//pre fix increment operator ++b		
+				System.out.println(++b);//21
+				System.out.println(b);//21
+				System.out.println(++b);//22
+				
+	   //decrement operator		
+				
+		//post fix decrement operator
+				a=90;
+				System.out.println(a--);//90
+				System.out.println(a);//89
+				System.out.println(a--);//89
+		
+		//pre fix decrement operator	
+				b=70;
+				System.out.println(--b);//69
+				System.out.println(b);//69
+				System.out.println(--b);//68
+		
+				//compound assigment operator += -=  *= /= %=
+				int c=90;
+				
+				
+				c+=10;//c=c+10;
+				
+						
+				System.out.println(c);//100
+				
+						
+				a=100;
+				b=++a;
+				System.out.println(a);//101
+				System.out.println(b);//101*/
+					
+				
+				
+	}
+
+}
